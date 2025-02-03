fe5012859052a6c12774a7b7350be40140d19eff
Gokul200318 <165988049+Gokul200318@users.noreply.github.com>
2025-02-03 19:34:50 +0530

    problem 3

--- prbl3.java
+++ prbl3.java
@@ -1,11 +1,18 @@
+
+import java.util.Scanner;
+
 //Check whether an alphabet is vowel or consonant using if..else statement.
 public class prbl3 {
     public static void main(String[] args) {
-        if(ch == 'a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-            System.out.println("it is vowel");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the alphabet");
+        char ch =scan.next().charAt(0);
+        if(ch == 'a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
+           ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+            System.out.println(ch +" " + "it is vowel");
         }
         else{
-            System.out.println("it is consonant");
+            System.out.println(ch + " " + "it is consonant");
         }
     }
 }
