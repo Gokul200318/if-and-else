3377e141c01d5f559256f18bfdea7367f41278d8
Gokul200318 <165988049+Gokul200318@users.noreply.github.com>
2025-02-03 19:18:47 +0530

    problem 3

---
+++ prbl3.java
@@ -0,0 +1,11 @@
+//Check whether an alphabet is vowel or consonant using if..else statement.
+public class prbl3 {
+    public static void main(String[] args) {
+        if(ch == 'a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+            System.out.println("it is vowel");
+        }
+        else{
+            System.out.println("it is consonant");
+        }
+    }
+}
