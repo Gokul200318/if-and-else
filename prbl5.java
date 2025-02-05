0ce83385686876e05cb11b9955ad20cac92772c9
Gokul200318 <165988049+Gokul200318@users.noreply.github.com>
2025-02-05 10:54:11 +0530

    problem 5

---
+++ prbl5.java
@@ -0,0 +1,14 @@
+
+public class prbl5 {
+    public static void main(String[] args) {
+        int[] n = {1,2,3,4,5,6};
+        int sum=0;
+        
+        for (int num:n){
+            sum = sum + num;
+        }
+        System.out.println(sum);
+
+
+    }
+}
